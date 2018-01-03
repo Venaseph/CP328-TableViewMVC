38eed5b04d14cd1df83f2541dbcd3247aaf91573
Chris Peragine <chrisperagine@Chriss-Mac.local>
2018-01-03 12:13:48 -0800

    Setup Commit

---
+++ TableViewMVC/TableViewMVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewMVC
+//
+//  Created by Chris Peragine on 1/3/18.
+//  Copyright © 2018 Chris Peragine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
