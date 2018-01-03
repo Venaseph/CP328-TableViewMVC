9b5f5404ef04736f75da071fd8310e0da6b1736c
Chris Peragine <chrisperagine@Chriss-Mac.local>
2018-01-03 12:41:46 -0800

    TableViews Complete, with Russian Numbering

--- TableViewMVC/TableViewMVC/ViewController.swift
+++ TableViewMVC/TableViewMVC/ViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let nums = ["ноль", "один","два","три","четыре","пять","шесть","семь","восемь", "девять"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return(nums.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "protoCell")
+        cell.textLabel?.text = nums[indexPath.row]
+        
+        return(cell)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
+    
 
 }
 
